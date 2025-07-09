db7ca8534b120fd8ebf67f2f335d36f3550876bf
ElKruzado <sa.acevedo@duocuc.cl>
2025-07-08 20:22:13 -0400

    agregar script de integrantes

---
+++ integrantes.py
@@ -0,0 +1,10 @@
+# integrantes.py
+integrantes = [
+    "Samuel Acevedo",
+    "Cristobal Venegas",
+    "Manuel Olate"
+]
+
+print("Integrantes del grupo:")
+for persona in integrantes:
+    print(persona)
